61df79e794f8692f4fdc88919d618954ac0e5f38
Coen Jacobs <coenjacobs@gmail.com>
2015-09-09 08:38:11 +0200

    Basic unit tests to test plugin function behaviour

---
+++ tests/DefaultBehaviourTest.php
@@ -0,0 +1,40 @@
+<?php
+
+class DefaultBehaviourTest extends PHPUnit_Framework_TestCase {
+	/** @test */
+	public function it_returns_same_array_when_free_shipping_is_not_set()
+	{
+		$input = array(
+			'test'            => 'test value',
+			'another_test'    => 'another_test_value',
+			'and_another_one' => 'more test values'
+		);
+		$return = wcfsd_woocommerce_package_rates( $input );
+		$this->assertEquals( $input, $return );
+	}
+
+	/** @test */
+	public function it_returns_only_free_shipping_when_it_is_the_only_one_available()
+	{
+		$input = array(
+			'free_shipping' => 'free_shipping_value'
+		);
+		$return = wcfsd_woocommerce_package_rates( $input );
+		$this->assertEquals( $input, $return );
+	}
+
+	/** @test */
+	public function it_returns_only_free_shipping_when_it_is_set()
+	{
+		$input = array(
+			'test'            => 'test value',
+			'free_shipping'   => 'free_shipping_value',
+			'and_another_one' => 'more test values'
+		);
+		$return = wcfsd_woocommerce_package_rates( $input );
+		$expected = array(
+			'free_shipping' => 'free_shipping_value'
+		);
+		$this->assertEquals( $expected, $return );
+	}
+}
